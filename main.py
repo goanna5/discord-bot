b263206425661a739b3c31f2d79afa21b737ae67
goanna5 <143087684+goanna5@users.noreply.github.com>
2024-04-05 14:31:31 +1000

    integrated slash commands!!! need to fix the option one to a selecting thing where you can select either count or age, not type it in, but everything else works!!

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 # adapted from https://www.freecodecamp.org/news/create-a-discord-bot-with-python/
+# and https://dev.to/mannu/ 
+# and https://stackoverflow.com/questions/71165431/how-do-i-make-a-working-slash-command-in-discord-py
 import discord
 import os
 from dog import *
 from ageify import *
 from dotenv import load_dotenv
+from discord import app_commands
 load_dotenv()
 
 intents = discord.Intents.all()
@@ -11,11 +14,57 @@ intents.messages = True
 intents.presences = True
 
 client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
+
+# help
+@tree.command(
+    name="help",
+    description="Will help you!!",
+    guild=discord.Object(id=1160760529879584808)
+)
+
+async def help_command(interaction):
+    await interaction.response.send_message("Help:\n\n**/ageify count||age"
+                                            + "*name*** - will return number "
+                                            + "of people with a certain name or the average "
+                                            + "age of people with that name\n*e.g. /ageify count "
+                                            + "Anna or /ageify age Anna*\n\n**/dog** "
+                                            + "- will return a random dog picture from the dog "
+                                            + "API\n\n**/help** - shows you this command as you know")
+
+# ageify
+@tree.command(
+    name="ageify",
+    description="Shows the number of people or average age of people with that name",
+    guild=discord.Object(id=1160760529879584808)
+)
+
+@app_commands.describe(name="Enter a name",
+                       option="What you want to see (count or age)")
+async def ageify_command(interaction, option: str, name: str):
+    if option in ["count", "age"]:
+        await interaction.response.send_message(ageify(name, option))
+    else:
+        await interaction.response.send_message("Must specify either \"count\" or \"age\"" +
+                                                " specifically in the option field")
+        
+# dog
+@tree.command(
+    name="dog",
+    description="Cute dog pic anyone?",
+    guild=discord.Object(id=1160760529879584808)
+)
+
+async def dog_command(interaction):
+    await interaction.response.send_message(getDog())
 
 @client.event
 async def on_ready():
     print('Logged in as {0.user}'.format(client))
+    # await client.tree.sync()
+    await tree.sync(guild=discord.Object(id=1160760529879584808))
 
+# checking for greetings to respond to
 @client.event
 async def on_message(message):
     print(f"Received message: {message.content}")
@@ -33,27 +82,6 @@ async def on_message(message):
 
     elif 'goanna' in message.content.lower() and message.author.name != 'a solitary goanna':
         await message.channel.send('You summoned the goanna\nRAHHHHHHH\n🦎🦎🦎🦎🦎🦎🦎🦎')
-
-    # API commands (note it starts with /g)
-    if message.content.startswith('/g dog'):
-        await message.channel.send(getDog())
-
-    if message.content.lower().startswith('/g name'):
-        # if in form '/g name Michael age' will return the average age of people w that name
-        # if in form '/g name Michael count' will return the number of people with the name
-        name = message.content.split(" ")[2]
-        value = message.content.lower().split(" ")[3]
-        if value in ["age\n", "count\n", "age", "count"]:
-            await message.channel.send(ageify(name, value))
-        else:
-            await message.channel.send("Must specify either age or count. Type \"/g help\" to get more info")
-
-    # help
-    if message.content.lower().startswith('/g help'):
-        await message.channel.send("Help:\n\n**/g name *your-name-goes-here* count||age** - " +
-                                   "e.g. /g name Anna count  or  /g name Anna age\nWill return number of people with the name Anna " +
-                                   "or the average age of people with the name Anna\n\n" +
-                                   "**/g dog** - will return a random dog picture from the dog API")
 
 print("Token from environment variable:", os.getenv('TOKEN'))
 
